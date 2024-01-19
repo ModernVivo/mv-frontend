6b057b11388863958c334e1f327f6cfdc3a39fde
huylesitdn <mrsjiodev@gmail.com>
2024-01-19 11:33:03 +0700

    update

--- src/features/results/components/no-results.tsx
+++ src/features/results/components/no-results.tsx
@@ -1,6 +1,6 @@
 export default function NoResult() {
   return (
-    <section className="mx-5 flex h-full w-screen flex-col items-center justify-center pt-40 mb-[15rem]">
+    <section className="mx-5 flex h-full flex-col items-center justify-center pt-40 mb-[15rem]">
       <h1 className="mb-20 text-center text-6xl tracking-[-1px]">
         No results found. Please try again.
       </h1>
