03c0358738359b997d2b133d4732ebfd54e17d2b
huylesitdn <mrsjiodev@gmail.com>
2024-01-29 14:29:41 +0700

    #1

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,2 @@
-export const API_ENDPOINT = "https://api.modernvivo.com/api/";
+// export const API_ENDPOINT = "https://api.modernvivo.com/api/";
+export const API_ENDPOINT = "http://localhost:8000/api/";
