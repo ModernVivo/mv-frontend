5ce1a5890d341a40f0f153c2cd0b7df06deedffe
huylesitdn <mrsjiodev@gmail.com>
2024-03-13 21:13:34 +0700

    Sign up

--- src/features/signup-form/index.tsx
+++ src/features/signup-form/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 
 import Image from "next/image";
-import Link from 'next/link'
 import { useRouter } from 'next/router';
 import { get } from 'lodash';
 import { useForm, type SubmitHandler } from 'react-hook-form';
@@ -9,17 +8,23 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
 
 import { useAppDispatch } from '~/hooks/reduxHooks';
-import { doLogin } from '~/store/slices/authSlice';
-import type { LoginRequest } from "~/types/auth";
+import { doSignUp, doLogin } from '~/store/slices/authSlice';
+import type { SignUpRequest } from "~/types/auth";
 
-export default function LoginForm() {
+export default function SignUpForm() {
   const router = useRouter();
   const dispatch = useAppDispatch();
   const [isLoading, setLoading] = useState(false);
 
   const validationSchema = Yup.object().shape({
     username: Yup.string().required('Username is required'),
-    password: Yup.string().required('Password is required')
+    email: Yup.string().email("Email invalid!").required('Email is required'),
+    first_name: Yup.string().required('First Name is required'),
+    last_name: Yup.string().required('Last Name is required'),
+    password: Yup.string().required('Password is required').min(6, "Must be at least 6 characters!"),
+    confirmPassword: Yup.string().required("Confirm password required!")
+      .oneOf([Yup.ref('password'), ''], 'Confirm password does not match!')
+      .min(6, "Must be at least 6 characters!"),
   });
   const formOptions = { resolver: yupResolver(validationSchema) };
 
@@ -27,18 +32,20 @@ export default function LoginForm() {
   const { register, handleSubmit, setError, formState } = useForm(formOptions);
   const { errors } = formState;
 
-  const onSubmit: SubmitHandler<LoginRequest> = async (formData: LoginRequest): Promise<void> => {
+  const onSubmit: SubmitHandler<SignUpRequest> = async (formData: SignUpRequest): Promise<void> => {
     const { username, password } = formData;
+
     setLoading(true);
     setError('root.apiError', { message: "" });
-    await dispatch(doLogin({ username, password }))
+    await dispatch(doSignUp(formData))
       .unwrap()
-      .then(() => {
-        const returnUrl = get(router, 'query.returnUrl', '/') as string;
-        router.push(returnUrl);
+      .then(async () => {
+          // login 
+          await dispatch(doLogin({ username, password })).unwrap()
+          router.push('/');
       })
       .catch(() => {
-        setError('root.apiError', { message: "Username or password does not match!" });
+        setError('root.apiError', { message: "User Exist!" });
         setLoading(false);
       });
   }
@@ -61,26 +68,45 @@ export default function LoginForm() {
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Sign in to your account
+                Sign Up
               </h1>
-              <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit(onSubmit)}>
+              <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit(onSubmit)} autoComplete="off">
                 <div>
                   <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
                   <input type="text"{...register('username')} placeholder="Username" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                   <div className="text-red-800 text-sm">{errors.username?.message}</div>
                 </div>
                 <div>
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                  <input type="email" {...register('email')} placeholder="Email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                  <div className="text-red-800 text-sm">{errors.email?.message}</div>
+                </div>
+                <div>
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
+                  <input type="text"{...register('first_name')} placeholder="First Name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                  <div className="text-red-800 text-sm">{errors.first_name?.message}</div>
+                </div>
+                <div>
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
+                  <input type="text"{...register('last_name')} placeholder="Last Name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                  <div className="text-red-800 text-sm">{errors.last_name?.message}</div>
+                </div>
+                <div>
                   <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                   <input type="password" {...register('password')} placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                   <div className="text-red-800 text-sm">{errors.password?.message}</div>
+                </div>
+                <div>
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
+                  <input type="password" {...register('confirmPassword')} placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                  <div className="text-red-800 text-sm">{errors.confirmPassword?.message}</div>
                 </div>
                 {/* <div className="flex items-center justify-end">
                   <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                 </div> */}
                 <button disabled={isLoading} type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-                  Sign in
+                  Sign Up
                 </button>
-                <Link className="block w-full text-white bg-blue-300 hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" href="/sign-up">Sign Up</Link>
                 {!!apiError &&
                   <div className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 text-center" role="alert">{apiError}</div>
                 }
---
+++ src/pages/sign-up.tsx
@@ -0,0 +1,5 @@
+import SignUp from "~/features/signup-form";
+
+export default function SignUpPage() {
+  return <SignUp />;
+}
